a83b6cae33da79a05abe8af5797d4938968b0e5f
d_nekh <dnekh2000@gmail.com>
2022-03-06 22:14:41 +0900

    Added reverse feature

--- ViceVersa/urls.py
+++ ViceVersa/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('reversed/', views.reverse, name='reverse'),
 ]
--- ViceVersa/views.py
+++ ViceVersa/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'user_text': user_text, 'user_reversed_text': reversed_text})
